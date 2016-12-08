20847d7824beaf4dd5f2e9fb0a45c961f013c07e
ZhangPengGitHub <zp0016@qq.com>
2016-12-08 20:52:13 +0800

    eof

---
+++ eof/eof/eof.cpp
@@ -0,0 +1,36 @@
+// eof.cpp : 定义控制台应用程序的入口点。
+// crtl + z
+// " " + crtl + z
+// 1 + crtl + z
+// 1 + " " + 2 + crtl + z
+// crtl + d
+// " " + crtl + d
+// 1 + crtl + d
+// 1 + " " + 2 + crtl + d
+
+#include "stdafx.h"
+#include "iostream"
+
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	int a, b; 
+	/*while (scanf("%d %d", &a, &b)!=-1) {
+		printf("%d\n", a + b); 
+	}*/
+	/*while (scanf("%d %d", &a, &b)!=0) {
+		printf("%d\n", a + b); 
+	}*/
+	/*while (1) {
+		scanf("%d %d", &a, &b);
+		printf("%d\n", a + b); 
+	}*/
+	int i = 0;
+	/*while (i<10) {
+		cout<<scanf("%d %d", &a, &b)<<endl;
+		i++;
+	}*/
+	return 0;
+}
+
